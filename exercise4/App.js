e68598d99034d525c0cd4015236be1e971fec3bb
Nilongg <niilo.vanhatupa@outlook.com>
2025-03-11 13:30:40 +0200

    Last exercises

---
+++ exercise4/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Button, Linking } from 'react-native';
+import * as Location from 'expo-location';
+import Camera from 'expo-camera'; // For permission functions
+
+export default function App() {
+  const [locationPermission, setLocationPermission] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [updateLocation, setUpdateLocation] = useState(false);
+
+  const getLocationPermission = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === 'granted') {
+      setLocationPermission(true);
+    } else {
+      console.log('Sijaintilupaa ei myönnetty');
+    }
+  };
+
+  const getLocation = async () => {
+    if (!locationPermission) return;
+    const locationData = await Location.getCurrentPositionAsync({});
+    setLocation({
+      latitude: locationData.coords.latitude,
+      longitude: locationData.coords.longitude
+    });
+  };
+
+  useEffect(() => {
+    getLocationPermission();
+    if (updateLocation) {
+      getLocation();
+      setUpdateLocation(false);
+    }
+  }, [updateLocation]);
+
+  return (
+    <View style={styles.container}>
+      <Button title="Open React Native Docs" onPress={() => Linking.openURL("https://reactnative.dev")} />
+      <Button title="Open Tampere On maps" onPress={() => Linking.openURL("geo:61.4978,23.7610")} />
+      <Button title="Get location" onPress={() => setUpdateLocation(true)} />
+
+      {location ? (
+        <Text>Latitude: {location.latitude}, Longitude: {location.longitude}</Text>
+      ) : (
+        <Text>No location data available</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', gap: 20},
+  text: { fontSize: 24, fontWeight: 'bold' },
+});
